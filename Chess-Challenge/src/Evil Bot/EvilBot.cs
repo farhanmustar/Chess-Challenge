f50360b774466abf37ef788b2c3a5bd430e952c7
Farhan Mustar <farhanmustar@gmail.com>
2023-07-29 01:17:25 +0800

    Update EvilBot to bot-1.

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -7,48 +7,56 @@ namespace ChessChallenge.Example
     // Plays randomly otherwise.
     public class EvilBot : IChessBot
     {
-        // Piece values: null, pawn, knight, bishop, rook, queen, king
-        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+      // Piece values: null, pawn, knight, bishop, rook, queen, king
+      int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
 
-        public Move Think(Board board, Timer timer)
-        {
-            Move[] allMoves = board.GetLegalMoves();
+      public Move Think(Board board, Timer timer)
+      {
+          Move[] allMoves = board.GetLegalMoves();
 
-            // Pick a random move to play if nothing better is found
-            Random rng = new();
-            Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
-            int highestValueCapture = 0;
+          // Pick a random move to play if nothing better is found
+          Random rng = new();
+          Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
+          int highestValueCapture = 0;
 
-            foreach (Move move in allMoves)
-            {
-                // Always play checkmate in one
-                if (MoveIsCheckmate(board, move))
-                {
-                    moveToPlay = move;
-                    break;
-                }
+          foreach (Move move in allMoves)
+          {
+              // Always play checkmate in one
+              if (MoveIsCheckmate(board, move))
+              {
+                  moveToPlay = move;
+                  break;
+              }
 
-                // Find highest value capture
-                Piece capturedPiece = board.GetPiece(move.TargetSquare);
-                int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
+              // Always promote to queen
+              if (move.IsPromotion && move.PromotionPieceType == PieceType.Queen) {
+                moveToPlay = move;
+                break;
+              }
 
-                if (capturedPieceValue > highestValueCapture)
-                {
-                    moveToPlay = move;
-                    highestValueCapture = capturedPieceValue;
-                }
-            }
+              // Find highest value capture
+              int movedPieceValue = pieceValues[(int)move.MovePieceType];
+              int capturedPieceValue = pieceValues[(int)move.CapturePieceType];
+              if (capturedPieceValue > 0 && board.SquareIsAttackedByOpponent(move.TargetSquare)) {
+                capturedPieceValue = capturedPieceValue - movedPieceValue;
+              }
+              if (capturedPieceValue > highestValueCapture)
+              {
+                  moveToPlay = move;
+                  highestValueCapture = capturedPieceValue;
+              }
+          }
 
-            return moveToPlay;
-        }
+          return moveToPlay;
+      }
 
-        // Test if this move gives checkmate
-        bool MoveIsCheckmate(Board board, Move move)
-        {
-            board.MakeMove(move);
-            bool isMate = board.IsInCheckmate();
-            board.UndoMove(move);
-            return isMate;
-        }
+      // Test if this move gives checkmate
+      bool MoveIsCheckmate(Board board, Move move)
+      {
+          board.MakeMove(move);
+          bool isMate = board.IsInCheckmate();
+          board.UndoMove(move);
+          return isMate;
+      }
     }
 }
